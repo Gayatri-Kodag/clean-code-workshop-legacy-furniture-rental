1a3b32ad1935df87fb95b9e88c72de44e910def0
Gayatri Kodag <gayatri.kodag@thoughtworks.com>
2023-11-27 23:03:42 +0530

    Step 1: Add characterization test for statement method

--- src/test/java/com/thoughtworks/furniturerental/CustomerTest.java
+++ src/test/java/com/thoughtworks/furniturerental/CustomerTest.java
@@ -13,4 +13,24 @@ public class CustomerTest {
         assertEquals(expected, 1 + 2);
     }
 
+    @Test
+    void shouldGenerateStatement() {
+        // Arrange
+        Customer customer = new Customer("John Doe");
+        Rental chairRental = new Rental(new Furniture("Chair", 0), 0);
+        Rental tableRental = new Rental(new Furniture("Table", 1), 0);
+        customer.addRental(chairRental);
+        customer.addRental(tableRental);
+        String expectedStatement = "Rental Record for John Doe\n" +
+                "\tChair\t200.0\n" +
+                "\tTable\t0.0\n" +
+                "Amount owed is 200.0\n" +
+                "You earned 2 frequent renter points";
+
+        // Act
+        String actualStatement = customer.statement();
+
+        // Assert
+        assertEquals(expectedStatement, actualStatement);
+    }
 }
