0808fc7fe9f6c73ab22935f8f0e9dd8ea7e0e55f
Gayatri Kodag <gayatri.kodag@thoughtworks.com>
2023-12-01 22:44:11 +0530

    Step 1.1 - Move the amount method to Furniture class

--- src/main/java/com/thoughtworks/furniturerental/Furniture.java
+++ src/main/java/com/thoughtworks/furniturerental/Furniture.java
@@ -25,4 +25,23 @@ public class Furniture {
         return title;
     }
 
+    public double amountFor(int daysRented) {
+        double amount = 0;
+        switch (getPriceCode()) {
+            case REGULAR:
+                amount += 200;
+                if (daysRented > 2)
+                    amount += (daysRented - 2) * 150;
+                break;
+            case NEW_LAUNCH:
+                amount += daysRented * 300;
+                break;
+            case CHILDREN:
+                amount += 150;
+                if (daysRented > 3)
+                    amount += (daysRented - 3) * 150;
+                break;
+        }
+        return amount;
+    }
 }
--- src/main/java/com/thoughtworks/furniturerental/Rental.java
+++ src/main/java/com/thoughtworks/furniturerental/Rental.java
@@ -19,23 +19,7 @@ public class Rental {
     }
 
     public double amount() {
-        double amount = 0;
-        switch (getFurniture().getPriceCode()) {
-            case Furniture.REGULAR:
-                amount += 200;
-                if (getDaysRented() > 2)
-                    amount += (getDaysRented() - 2) * 150;
-                break;
-            case Furniture.NEW_LAUNCH:
-                amount += getDaysRented() * 300;
-                break;
-            case Furniture.CHILDREN:
-                amount += 150;
-                if (getDaysRented() > 3)
-                    amount += (getDaysRented() - 3) * 150;
-                break;
-        }
-        return amount;
+        return furniture.amountFor(daysRented);
     }
 
     public int frequentRenterPoints() {
