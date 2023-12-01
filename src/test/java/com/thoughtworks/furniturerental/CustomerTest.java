a0e932622cdba4589cf8054b84408963627474ca
Gayatri Kodag <gayatri.kodag@thoughtworks.com>
2023-12-01 22:06:44 +0530

    Step 1 - Add characterization test

--- src/test/java/com/thoughtworks/furniturerental/CustomerTest.java
+++ src/test/java/com/thoughtworks/furniturerental/CustomerTest.java
@@ -13,4 +13,26 @@ public class CustomerTest {
         assertEquals(expected, 1 + 2);
     }
 
+    @Test
+    void shouldGenerateStatement() {
+        Customer customer = new Customer("John Doe");
+        Rental chairRental = new Rental(new Furniture("Chair", 0), 2);
+        Rental tableRental = new Rental(new Furniture("Table", 1), 3);
+        Rental wardrobeRental = new Rental(new Furniture("Wardrobe", 2), 6);
+        customer.addRental(chairRental);
+        customer.addRental(tableRental);
+        customer.addRental(wardrobeRental);
+        String expectedStatement="Rental Record for John Doe\n"+
+                "\tChair\t200.0\n"+
+                "\tTable\t900.0\n"+
+                "\tWardrobe\t600.0\n"+
+                "Amount owed is 1700.0\n"+
+                "You earned 4 frequent renter points";
+
+        String actualStatement = customer.statement();
+
+
+        assertEquals(expectedStatement, actualStatement);
+
+    }
 }
