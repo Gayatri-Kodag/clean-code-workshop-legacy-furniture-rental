75967d8d356608b4649584c2ed0933ed046e575c
Gayatri Kodag <gayatri.kodag@thoughtworks.com>
2023-12-01 22:28:54 +0530

    Step 4.1 Extract the total rental amount and replace temp variable with expression

--- src/main/java/com/thoughtworks/furniturerental/Customer.java
+++ src/main/java/com/thoughtworks/furniturerental/Customer.java
@@ -20,7 +20,6 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
         int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
         for (Rental rental : rentals) {
@@ -29,14 +28,21 @@ public class Customer {
             //show figures for this rental
             result += "\t" + rental.getFurniture().getTitle() + "\t" +
                     rental.amount() + "\n";
-            totalAmount += rental.amount();
         }
 
         //add footer lines result
-        result += "Amount owed is " + totalAmount + "\n";
+        result += "Amount owed is " + totalRentalAmount() + "\n";
         result += "You earned " + frequentRenterPoints
                 + " frequent renter points";
         return result;
+    }
+
+    private double totalRentalAmount() {
+        double totalAmount = 0;
+        for (Rental rental : rentals) {
+            totalAmount += rental.amount();
+        }
+        return totalAmount;
     }
 
 }
