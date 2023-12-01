718d854e503cd3b4df4ec5dc1c32680cfd02cfd9
Gayatri Kodag <gayatri.kodag@thoughtworks.com>
2023-12-01 23:01:40 +0530

    Step 4.1 Override the amountFor method in subclasses and make the method abstract in FurniturePriceCode class

--- src/main/java/com/thoughtworks/furniturerental/NewLaunchFurniturePriceCode.java
+++ src/main/java/com/thoughtworks/furniturerental/NewLaunchFurniturePriceCode.java
@@ -5,4 +5,9 @@ public class NewLaunchFurniturePriceCode extends FurniturePriceCode{
     int getPriceCode() {
         return Furniture.NEW_LAUNCH;
     }
+
+    @Override
+    double amountFor(int daysRented) {
+        return daysRented * 300;
+    }
 }
--- src/main/java/com/thoughtworks/furniturerental/RegularFurniturePriceCode.java
+++ src/main/java/com/thoughtworks/furniturerental/RegularFurniturePriceCode.java
@@ -5,4 +5,13 @@ public class RegularFurniturePriceCode extends FurniturePriceCode{
     int getPriceCode() {
         return Furniture.REGULAR;
     }
+
+    @Override
+    double amountFor(int daysRented) {
+        double amount = 0;
+        amount += 200;
+        if (daysRented > 2)
+            amount += (daysRented - 2) * 150;
+        return amount;
+    }
 }
